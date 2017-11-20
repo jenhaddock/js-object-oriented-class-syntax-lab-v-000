0cc0e4a9db5d1a79e12fbc7523fc74c11eb8844c
Jennifer Haddock <jentx123@hotmail.com>
2017-11-20 00:11:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ class BoardMember{
   sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
-};
+}
 
-class Ceo extends User{
+class Ceo extends BoardMember{
   hireEmployee(){
     return 'Welcome aboard!';
   }
